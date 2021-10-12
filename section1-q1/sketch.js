6e28f31d584f5821282444aa07b2a031d16c9be1
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-10-12 12:29:56 +0900

    Change section1-q1 (1)

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -7,7 +7,9 @@ function setup(){
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  stroke(0,128,0); 
+  arc(100 + 25, 100, 100, 100, HALF_PI+ QUARTER_PI , QUARTER_PI * 3 + PI);
+  arc(100 - 28, 100, 100, 100, QUARTER_PI + PI, PI * 2 + QUARTER_PI);
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
