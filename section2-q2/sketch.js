caced2b5dd53781194e2698b4dc4f2654325c839
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-10-19 13:30:47 +0900

    change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,21 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if((i + j) % 2 == 0){
+        fill(255);
+      }
+      else {
+        fill(120);
+      }
+      rect(i * size, j * size, size, size);
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    if((i + j) % 2 == 1 && j < 3){
+      fill(255, 0, 0);
     }
+    else if((i + j) % 2 == 1 && j > 4){
+      fill(0);
+    }
+    ellipse(i * size + size / 2, j * size + size / 2, size - 4, size - 4);
+  }
   }
 }
