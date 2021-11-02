a31551628b3fb2791d6a9df1d0490e27c5fcb760
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-11-02 11:10:38 +0900

    change3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
@@ -12,5 +13,21 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  let size = count
+
+  if(count < 48){
+    ellipse(width / 2, height / 2, 2 * size);
+  }
+  else if(count >= 52){
+    ellipse(width / 2, height / 2, (100 - size) * 2);
+  }
+  else{
+    ellipse(width / 2, height / 2, 96);
+  }
+
+  if(keyIsDown("Q".charCodeAt(0))){
+    count += 5;
+  }
+  
+
 }
