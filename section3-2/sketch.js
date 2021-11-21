0b9d3fa18537b79ea824ea92ff466225feeb43e8
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-11-21 15:22:33 +0900

    change section3-2(mario)

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -1,11 +1,18 @@
 // テキスト「アニメーションの基本」
-let x, y, vx, vy;
-const g = 1; // 重力加速度
+let x, y, vx, vy, img;
+
+function preload() {
+  img = loadImage('mario.png');
+}
+const g = 2; // 重力加速度
 const vyMax = 30;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
+  loadImage('mario.png', img => {
+    image(img, 0, 0);
+  });
+  x = width / 10;
   y = height / 2;
   vx = 8;
   vy = 8;
@@ -13,25 +20,34 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 20, 20);
-  x += vx;
+  image(img, x, y, 40, 40);
+
+    if(keyIsDown("B".charCodeAt(0)) && keyIsDown(LEFT_ARROW)){ x -= vx * 2; }
+    else if(keyIsDown(LEFT_ARROW)){ x -= vx; }
+    if(keyIsDown("B".charCodeAt(0)) && keyIsDown(RIGHT_ARROW)){ x += vx * 2; }
+    else if(keyIsDown(RIGHT_ARROW)){ x += vx; }
+    if(y >= height - 40 && keyIsDown(" ".charCodeAt(0))){ vy = -30; }
+
   y += vy;
 
-  // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
   vy = constrain(vy + g, -vyMax, vyMax);
+   y = constrain(y, 0, height - 40);
 
-  // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+  if(x > width){ x = 0; }
+  else if(x < 0){ x = width; }
+  if(y > height - 40){ vy = 0; }
+
 
 　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
+  // if(x < 0 || x > width){ vx = -1 * vx; }
+  // if(y > height){ vy = 0 }
+  // x = constrain(x, 0, width);
+  // y = constrain(y, 0, height - 8);
+
+
 }
+
+
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
