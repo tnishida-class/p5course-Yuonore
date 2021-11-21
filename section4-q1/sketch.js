a91c3cc4780833d6cb6c7dbfa6a4776dff13ddcc
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-11-21 16:41:50 +0900

    change seqtion4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -17,8 +17,18 @@ function setup(){
   // ここからが本番
   fill(0);
   const dx = width / scores.length;
-  let px, py; // 線を引くために一つ前の点を覚えておく変数
+
+  let px , py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
+  strokeWeight(8)
+  point(dx * (i + 0.5), (100 - scores[i]) * height / 100)
+    px = i - 1
+    py = scores[i - 1]
+
+  if(i >= 1){
+    strokeWeight(1)
+　  line(dx * (px + 0.5), (100 - py) * height / 100, dx * (i + 0.5), (100 - scores[i]) * height / 100)
   }
+}
 }
