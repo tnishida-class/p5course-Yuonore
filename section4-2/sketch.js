56c8a1ca1497d44a4945f09f3b057162501ad208
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-11-29 14:14:23 +0900

    change section4-2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -22,8 +22,9 @@ function draw(){
 function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
+  let s = random(5, 30)
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: s, vx: dx, vy: dy };
     balls.push(b);
   }
 }
