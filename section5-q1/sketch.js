486a34a04b44279f51b126fac6f1bede72ffe641
Yuonore <91930412+Yuonore@users.noreply.github.com>
2021-11-29 16:49:14 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,19 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 50, 100, 0, 0, 255);
 }
 
-function balloon(t){
+function balloon(t, x, y, r, g, b){
+  push();
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  noStroke();
+  fill(r, g, b);
+  rect(x, y, w + p * 2, h + p * 2);
+  triangle(x + w + p * 2 , y + h + p * 2, x + w + p * 2 , y + h + p * 2 + 10, x + w + p * 2 - 10, y + h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, x + p, y + h + p);
+  pop();
 }
